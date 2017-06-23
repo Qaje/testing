8c57408fb90f440bfa619e3b10e9213c39dff6f1
qaje <javierpal3232@gmail.com>
2017-06-23 18:31:04 -0400

    second make tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-use App\Task;
+use Testing\Task;
 use Illuminate\Http\Request;
 
 /*
@@ -21,7 +21,12 @@ Route::get('/', function () {
  * Display All Tasks
  */
 Route::get('/', function(){
-	return view('tasks');
+	$tasks = Task::orderBy('created_at','asc')->get();
+
+
+	return view('tasks'.[
+	'tasks' => $tasks
+		]);
 });
 
 /**
@@ -31,12 +36,18 @@ Route::post('/task', function (Request $request) {
     $validator = Validator::make($request->all(),[
     'name' => 'required|max:255',
     ]);
-    
+
       if ($validator->fails()) {
         return redirect('/')
             ->withInput()
             ->withErrors($validator);
     }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect ('/');
 
     // Create The Task...
 });
